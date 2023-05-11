bf80cb8e1b6b5c16cd8b4c3f1876c04d970eacc1
Shabbir1065 <shabbir1065@gmail.com>
2023-05-10 20:52:04 -0400

    things are coming together! Almost everything works, just need to add functionality to update button. Delete button works fully, information is displayed for the user, and adding items is perfect.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios"
 
 export const Home = () => {
+    const [incomes, setIncomes] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+    const [investments, setInvestments] = useState([]);
+
+    useEffect(() => {
+        const userID = window.localStorage.getItem("userID");
+        const fetchIncomes = async () => {
+            try{
+                //section for incomes
+                const incomeResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/income`);
+                setIncomes(incomeResponse.data);
+
+                //section for expenses
+                const expenseResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/expense`);
+                setExpenses(expenseResponse.data);
+
+                //section for investments
+                const investmentResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/investment`);
+                setInvestments(investmentResponse.data);
+            }
+            catch(err){
+                console.error(err);
+            }
+        };
+        
+        fetchIncomes();
+    }, []);
 
     return (
     <div>
-        <h2>Enter your incomes, expenses, and investments:</h2>
-        <UserInput/>
-        <FinanceSummary/>
+        <h2 className="welcomeUser">Enter your incomes, expenses, and investments:</h2>
+        <UserInput
+        setIncomes={setIncomes}
+        setExpenses={setExpenses}
+        setInvestments={setInvestments}
+        />
+        <FinanceSummary
+        incomes={incomes}
+        expenses={expenses}
+        investments={investments}
+        setIncomes={setIncomes}
+        setExpenses={setExpenses}
+        setInvestments={setInvestments}
+        />
     </div>
     );
 }
 
-const UserInput = () => {
+const UserInput = ({setIncomes, setExpenses, setInvestments}) => {
     const [description, setDescription] = useState("");
     const [value, setValue] = useState("");
     const [financeType, setFinanceType] = useState("income");
@@ -22,9 +60,20 @@ const UserInput = () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         try{
-            const response = await axios.post("http://localhost:3001/api", {userID, "finances": [{description, value, financeType}]});
-            console.log(response.data);
-            alert("Added to the database!"); //MEANT ONLY FOR TESTING. DELETE LATER.
+            await axios.post("http://localhost:3001/api", {userID, "finances": [{description, value, financeType}]});
+            //---------RE-RENDER STUFF--------
+            //section for incomes
+            const incomeResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/income`);
+            setIncomes(incomeResponse.data);
+
+            //section for expenses
+            const expenseResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/expense`);
+            setExpenses(expenseResponse.data);
+
+            //section for investments
+            const investmentResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/investment`);
+            setInvestments(investmentResponse.data);
+            
         } catch (err){
             console.error(err);
         }
@@ -60,17 +109,70 @@ const UserInput = () => {
     );
 }
 
-const FinanceSummary = () => {
+const FinanceSummary = ({incomes, expenses, investments, setIncomes, setExpenses, setInvestments}) => {
+    const userID = window.localStorage.getItem("userID");
+
+    const deleteItem = async (financeID) => {
+        try{
+            console.log(userID);
+            console.log(financeID);
+            const response = await axios.delete("http://localhost:3001/api/deleteFinance", {data: {userID, financeID}});
+            console.log(response.data);
+            //---------RE-RENDER STUFF--------
+            //section for incomes
+            const incomeResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/income`);
+            setIncomes(incomeResponse.data);
+
+            //section for expenses
+            const expenseResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/expense`);
+            setExpenses(expenseResponse.data);
+
+            //section for investments
+            const investmentResponse = await axios.get(`http://localhost:3001/api/userFinanceByType/${userID}/investment`);
+            setInvestments(investmentResponse.data);
+        }
+        catch(err){
+            console.error(err);
+        }
+    };
+
     return(
         <div className="financeSummary">
             <div className="incomes finance-section">
                 <h3>Incomes:</h3>
+                <div className="finance-ul-items">
+                    {incomes.map((income) => (
+                        <div className="finance-single-item" key={income._id}>
+                            <h4>${income.value} from {income.description}</h4>
+                            <button className="updateButton">Update</button>
+                            <button className="deleteButton" onClick={() => deleteItem(income._id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="expenses finance-section">
                 <h3>Expenses</h3>
+                <div className="finance-ul-items">
+                    {expenses.map((expense) => (
+                        <div className="finance-single-item" key={expense._id}>
+                            <h4>${expense.value} from {expense.description}</h4>
+                            <button className="updateButton">Update</button>
+                            <button className="deleteButton" onClick={() => deleteItem(expense._id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="investments finance-section">
                 <h3>Investments</h3>
+                <div className="finance-ul-items">
+                    {investments.map((investment) => (
+                        <div className="finance-single-item" key={investment._id}>
+                            <h4>${investment.value} from {investment.description}</h4>
+                            <button className="updateButton">Update</button>
+                            <button className="deleteButton" onClick={() => deleteItem(investment._id)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="breakdown finance-section">
                 <h3>Breakdown:</h3>
